c11398cc370352f1a5bb14757327a7bc50b1f829
Lê Quang Tiệp <lequangtiep@MacBook-Air-cua-Le.local>
2024-03-14 08:32:59 +0700

    first commit

---
+++ baitap/ra/business/implement/StudentImplement.java
@@ -0,0 +1,52 @@
+package baitap.ra.business.implement;
+
+import baitap.ra.business.config.InputMethods;
+import baitap.ra.business.design.CRUD;
+import baitap.ra.business.entity.Student;
+
+public class StudentImplement implements CRUD {
+    private static Student[] students = new Student[100];
+    private int size = 0;
+    @Override
+    public void create() {
+        if (size == 100){
+            System.err.println("mảng đầy");
+        }else {
+            // logic thêm mới
+            // ta 1 đối tượng student
+            Student newStudent = new Student();
+            newStudent.inputData();
+            // thêm vào mang
+            students[size] = newStudent;
+            size++; // tăng số lương
+        }
+    }
+
+    @Override
+    public void display() {
+        if (size == 0){
+            System.err.println("mảng ko co ptu");
+        }else {
+            for (int i = 0; i < size; i++) {
+                System.out.println(students[i]);
+            }
+        }
+    }
+
+    @Override
+    public void edit() {
+        System.out.println("Nhập mã muốn sửa");
+        byte idEdit = InputMethods.getByte();
+        for (int i = 0; i < size; i++) {
+            if (students[i].getStudentId()==idEdit){
+                students[i].inputData();
+            }
+        }
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
